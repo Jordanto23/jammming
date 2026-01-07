6a9b57ffe0bc4488f6934a8e21648ab1e1589a0b
Jordan Kulas <jordankulas@comcast.net>
2026-01-07 13:44:08 -0500

    playlist can be renamed

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import Tracklist from "./Tracklist";
 
 function Playlist({ playlistName, setPlaylistName, playlistTracks, setPlaylistTracks }) {
-  const handleNameChange = (e) => {
-    setPlaylistName(e.target.value);
+
+  // Handler for when the user types a new name
+  const handleNameChange = (event) => {
+    setPlaylistName(event.target.value);
   };
 
   return (
     <div className="Playlist">
+      {/* Editable input for playlist name */}
       <input
+        type="text"
         value={playlistName}
         onChange={handleNameChange}
         placeholder="Playlist Name"
       />
+
+      {/* Render tracks in the playlist */}
       <Tracklist
         tracks={playlistTracks}
         playlistTracks={playlistTracks}
         setPlaylistTracks={setPlaylistTracks}
-        isRemoval={true} // - button for removing
+        isRemoval={true} // "-" button for removing tracks
       />
+
       <button onClick={() => alert("Saving playlist to Spotify!")}>
         Save to Spotify
       </button>
